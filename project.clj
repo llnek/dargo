7d2230bac65a2877e43021ede47de5f0ebc3ace5
Kenneth Leung <ccllnekk@gmail.com>
2018-02-07 00:33:42 -0800

    kl

---
+++ project.clj
@@ -0,0 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(defproject io.czlab/elmo "1.0.0"
+
+  :license {:url "http://www.eclipse.org/legal/epl-v10.html"
+            :name "Eclipse Public License"}
+
+  :description ""
+  :url "https://github.com/llnek/elmo"
+
+  :dependencies []
+
+  :plugins [[lein-shell "0.5.0"]]
+
+  :profiles {:provided {:dependencies [[org.clojure/clojure
+                                        "1.9.0" :scope "provided"]]}
+             :uberjar {:aot :all}}
+
+  :global-vars {*warn-on-reflection* true}
+  :target-path "out/%s"
+  :aot :all
+
+  :coordinate! "czlab"
+  :omit-source true
+
+  :aliases {"deploy" ["with-profile"
+                      "podify" "wabbit"]
+            "compile" ["shell" "bin/build"]
+            "run" ["trampoline"
+                   "run" "-m" "czlab.wabbit.core"]}
+
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;EOF
+
